d9febad52ded0ab0431b19f9ebb78381e62ba259
Kishor Naik <kishor.naik011.net@gmail.com>
2021-08-18 15:46:51 +0530

    Code Commit

---
+++ DemoRenderFragment/Sol_Demo/Sol_Demo/Pages/Components/When.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Components
+{
+    public partial class When
+    {
+        #region Props
+
+        [Parameter]
+        public bool Condition { get; set; }
+
+        [Parameter]
+        public RenderFragment IfRender { get; set; }
+
+        [Parameter]
+        public RenderFragment ElseRender { get; set; }
+
+        #endregion Props
+    }
+}
